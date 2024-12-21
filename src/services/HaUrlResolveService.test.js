d001a29cc9070d47dc5d49295bfcc850dcbd2bb3
Nathan Bijnens <nathan@nathan.gs>
2024-12-21 22:20:38 +0100

    UrlResolveService: extra test

--- src/services/HaUrlResolveService.test.js
+++ src/services/HaUrlResolveService.test.js
@@ -48,6 +48,23 @@ describe("HaUrlResolveService", () => {
       const url = "http://localhost:8123/{{ states('sensor.mySensor') }}/test/{{ states('sensor.myOtherSensor') }}/bla";
       expect(haUrlResolveService.resolveUrl(url)).toBe("http://localhost:8123/on/test//bla");
     });
+
+    it("should resolve URLs coming from linkedentityservice", () => {
+      const hass = {
+        states: {
+          "sensor.mySensor": { state: "on" },
+        },
+      };      
+      const haUrlResolveService = new HaUrlResolveService(hass, {});
+      const url = "http://localhost:8123/{{ states('sensor.mySensor') }}/test";
+      expect(haUrlResolveService.resolveUrl(url)).toBe("http://localhost:8123/on/test");
+
+      haUrlResolveService.states = {
+        "sensor.mySensor": "off"
+      };
+
+      expect(haUrlResolveService.resolveUrl(url)).toBe("http://localhost:8123/off/test");
+    });
   });
 
   describe("resolveEntities", () => {
