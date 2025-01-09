a973768c023fc70fc84cef4ee8b0ad5fc6090b41
Nathan Bijnens <nathan@nathan.gs>
2025-01-09 21:59:38 +0100

    Refactor Entity & EntitiesRenderService

--- src/models/Entity.js
+++ src/models/Entity.js
@@ -1,28 +1,57 @@
 
 import EntityHistory from "../models/EntityHistory.js";
-import L from 'leaflet';
-import { Polyline, CircleMarker } from "leaflet";
+import L, { LayerGroup, LatLng, Map } from "leaflet";
 import Logger from "../util/Logger.js"
 import EntityConfig from "../configs/EntityConfig.js";
+import HaHistoryService from "../services/HaHistoryService.js";
+import HaDateRangeService from "../services/HaDateRangeService.js";
+import HaLinkedEntityService from "../services/HaLinkedEntityService.js";
+import HaMapUtilities from "../util/HaMapUtilities.js";
 
 export default class Entity {
   /** @type {EntityConfig} */
   config;  
   /** @type {L.Marker} */
   marker;
-  /** @type {[string]} */
-  title;
-  /** @type {[EntityHistory]} */
-  histories = [];
-
+  /** @type {EntityHistory} */
+  history;
+  /** @type {LayerGroup} */
+  historyLayerGroup;
+  /** @type {object} */
+  hass;
+  /** @type {Map} */
+  map;
+  /** @type {string} */
   _currentState;
+  /** @type {HaHistoryService} */
+  historyService;
+  /** @type {HaDateRangeService} */
+  dateRangeManager;
+  /** @type {HaLinkedEntityService} */
+  linkedEntityService;
 
   // Set initial values
+  /** @type {Date} */
   currentHistoryStart;
+  /** @type {Date} */
   currentHistoryEnd;
 
   /** @type {[boolean]} */
   darkMode;
+  
+  constructor(config, hass, map, historyService, dateRangeManager, linkedEntityService, darkMode) {
+    this.config = config;
+    this.hass = hass;
+    this.map = map;
+    this.darkMode = darkMode;
+    this.historyService = historyService;
+    this.dateRangeManager = dateRangeManager;
+    this.linkedEntityService = linkedEntityService;
+
+    if(this.display == "state") {
+      this._currentState = this.title;
+    }    
+  }
 
   setHistoryDates(start, end){
     this.currentHistoryStart = start;
@@ -37,25 +66,66 @@ export default class Entity {
     return this.config.display;
   }
 
-  constructor(config, latitude, longitude, icon, title, state, picture, darkMode) {
-    this.config = config;
-    this.darkMode = darkMode;
-    if(this.display == "state") {
-      title = state;
-      this._currentState = state;
-    }
-    this.title = title;
-    this.marker = this._createMapMarker(latitude, longitude, icon, title, picture);
+  /** @returns {object} */
+  get state() {
+    return this.hass.states[this.id];
   }
 
-  _markerCss(size) {
-    return `style="height: ${size}px; width: ${size}px;"`;
+  get picture() {
+    // If no configured picture, fallback to entity picture
+    let picture = this.config.picture ?? this.state.attributes.entity_picture;
+    // Skip if neither found and return null
+    return picture ? this.hass.hassUrl(picture) : null;
   }
 
-  _abbr(title) {
-    if(this.display == "state") {
-      return title;
+  
+  /** @returns {LatLng} */
+  get latLng() {
+    if(this.config.fixedX && this.config.fixedY) {
+      return new LatLng(this.config.fixedX, this.config.fixedY);
     }
+    
+    // Do we have Lng/Lat directly?
+    if (this.state.attributes.latitude && this.state.attributes.longitude) {
+      return new LatLng(this.state.attributes.latitude, this.state.attributes.longitude);
+    }
+    
+    // Get Lat/Lng of entity. Some entities such as "person" define device_trackers allowing
+    // multiple lat/lng sources to be used. This method will call down through these looking for a
+    // lat/lng value if none is defined on the parent entity.
+    // If any, see if we can get a lng/lat from one instead
+    let subTrackerIds = this.state?.attributes?.device_trackers ?? []
+    for(let t=0; t<subTrackerIds.length; t++) {
+      const entity = this.hass.states[subTrackerIds[t]];
+      if (entity.attributes.latitude && entity.attributes.longitude) {
+        return new LatLng(entity.attributes.latitude, entity.attributes.longitude);
+      }
+    }    
+    
+    Logger.warn("Entity: " + this.id + " has no latitude & longitude");
+    if(this.config.fallbackX && this.config.fallbackY) {
+      return new LatLng(this.config.fallbackX, this.config.fallbackY);
+    }
+    Logger.error("Entity: " + this.id + " has no fallback latitude & longitude");
+    throw Error("Entity: " + this.id + " has no latitude & longitude and no fallback configured")
+  }
+
+  setup() {
+    this.marker = this._createMapMarker();
+    this.marker.addTo(this.map);
+    this.setupHistory();
+  }
+
+  get friendlyName() {
+    return this.state.attributes.friendly_name;
+  }
+
+  /** @returns {string} */
+  get title() {
+    if(this.display == "state") {
+      return this.hass.formatEntityState(this.state);;
+    }
+    const title = this.friendlyName;
     if(title.length < 5) {
       return title;
     }
@@ -67,52 +137,138 @@ export default class Entity {
       .toUpperCase();
   }
 
+  /** @returns {string} */
+  get tooltip() {
+    return this.friendlyName ?? "";
+  }
+
+  get icon() {
+    return this.config.icon ?? this.state.attributes.icon;
+  }
+
+  _markerCss(size) {
+    return `style="height: ${size}px; width: ${size}px;"`;
+  }
+
   get hasHistory() {
     return this.config.hasHistory;
   }
 
-  _latlong(latitude, longitude) {
-    if(this.config.fixedX && this.config.fixedY) {
-      return [this.config.fixedX, this.config.fixedY];
-    }
-    if(latitude == null || longitude == null) {
-      Logger.warn("Entity: " + this.id + " has no latitude & longitude");
-      if(this.config.fallbackX == null || this.config.fallbackY == null) {
-        Logger.error("Entity: " + this.id + " has no fallback latitude & longitude");
-        throw Error("Entity: " + this.id + " has no latitude & longitude and no fallback configured")
-      }
-    }
-    return [latitude ?? this.config.fallbackX, longitude ?? this.config.fallbackY];
-  }
-
-  update(map, latitude, longitude, state) {
+  async update() {
     if(this.display == "state") {
-      if(state != this._currentState) {
-        Logger.debug("[Entity] updating marker for " + this.id + " from " + this._currentState + " to " + state);
+      if(this.state != this._currentState) {
+        Logger.debug("[Entity] updating marker for " + this.id + " from " + this._currentState + " to " + this.state);
         this.marker.remove();
-        this.marker = this._createMapMarker(latitude, longitude, null, state, null);
-        this.marker.addTo(map);
-        this._currentState = state;
+        this.marker = this._createMapMarker();
+        this.marker.addTo(this.map);
+        this._currentState = this.state;
       }
     }
-    this.marker.setLatLng(this._latlong(latitude, longitude));
+    this.marker.setLatLng(this.latLng);
+    this.renderHistory();
   }
 
-  setupHistory(historyService, start, end) {
-    if(this.hasHistory) {
-      const entHist = new EntityHistory(this.id, this.title, this.config.historyLineColor, this.config.gradualOpacity, this.config.historyShowDots, this.config.historyShowLines);
-      historyService.subscribe(entHist.entityId, start, end, entHist.retrieve, this.config.useBaseEntityOnly);
-      this.histories.push(entHist);
-    }  
+
+  refreshEntityHistory() {
+    Logger.debug(`[Entity] Refreshing history for ${this.id}: ${this.currentHistoryStart} -> ${this.currentHistoryEnd}`);
+    // Remove layer if it already exists.
+    if(this.historyLayerGroup) this.map.removeLayer(this.historyLayerGroup);
+
+    this.historyLayerGroup = new LayerGroup();
+    this.map.addLayer(this.historyLayerGroups[this.id]);
+
+    // Subscribe new history
+    this.setupEntityHistories(this.currentHistoryStart, this.currentHistoryEnd);
   }
 
-  /** @returns {[Polyline|CircleMarker]} */
+  setupHistory() {    
+    let historyDebug = `History config for: ${this.id}\n`;
+      
+    if (!this.hasHistory) {
+      historyDebug += `- Not enabled`;
+      Logger.debug(historyDebug);
+      return;
+    }
+
+    // Setup layer for entities history
+    this.historyLayerGroup = new LayerGroup();
+    this.map.addLayer(this.historyLayerGroup);
+
+    // If entity is using the date range manager.
+    if (this.config.usingDateRangeManager) {
+      // HaDateRangeService, HaLinkedEntityService and future services should use same structure.
+      this.dateRangeManager.onDateRangeChange((range) => {
+        this.setHistoryDates(range.start, range.end);
+        this.refreshEntityHistory(this.map, this.historyService);
+      });
+
+      historyDebug += `- Using DateRangeManager`;
+      Logger.debug(historyDebug);
+    }
+
+    // If have start entity, link it
+    if (this.config.historyStartEntity) {
+      this.linkedEntityService.onStateChange(
+        this.config.historyStartEntity,
+        (newState) => {
+            const date = HaMapUtilities.getEntityHistoryDate(newState, this.config.historyStartEntitySuffix);
+            this.setHistoryDates(date, this.currentHistoryEnd);
+            this.refreshEntityHistory(this.map, this.historyService);
+          }
+        );
+        historyDebug += `- Start: linked entity "${this.config.historyStartEntity}"\n`;
+      } else {
+        this.currentHistoryStart = this.config.historyStart;
+        historyDebug += `- Start: fixed date ${this.currentHistoryStart}\n`;
+      }
+
+      // If have end entity, link it.
+      if (this.config.historyEndEntity) {
+        this.linkedEntityService.onStateChange(
+          this.config.historyEndEntity,
+          (newState) => {
+            const date = HaMapUtilities.getEntityHistoryDate(newState, this.config.historyEndEntitySuffix);
+            this.setHistoryDates(this.currentHistoryStart, date);
+            this.refreshEntityHistory(this.map, this.historyService);
+          }
+        );
+        historyDebug += `- End: linked entity "${this.config.historyEndEntity}"\n`;
+      } else {
+        this.currentHistoryEnd = this.config.historyEnd;
+        historyDebug += `- End: fixed date ${this.currentHistoryEnd??'now'}\n`;
+      }
+
+      // Provide summary of config for each entities history
+      Logger.debug(historyDebug);
+
+      // Render history now if start is fixed and end isn't dynamic
+      if (this.config.historyStart && !this.config.historyEndEntity) {
+        this.setupHistory(this.config.historyStart, this.config.historyEnd);
+      }
+
+    
+      
+    }
+    
+  setupEntityHistories(start, end) {
+    this.history = new EntityHistory(this.id, this.tooltip, this.config.historyLineColor, this.config.gradualOpacity, this.config.historyShowDots, this.config.historyShowLines);
+    this.historyService.subscribe(this.history.entityId, start, end, this.history.retrieve, this.config.useBaseEntityOnly);
+  }
+
   renderHistory() {
-    return this.histories.map((entHist) => entHist.render()).flat();  
+    if(!this.hasHistory) {
+      return;
+    }
+    this.history?.render().flat().forEach((marker) => {
+      marker.addTo(this.historyLayerGroup);
+    });
   }
 
-  _createMapMarker(latitude, longitude, icon, title, picture) {
+  
+  _createMapMarker() {
     Logger.debug("[MarkerEntity] Creating marker for " + this.id + " with display mode " + this.display);
+    let icon = this.icon;
+    let picture = this.picture;
     if(this.display == "icon") {
       picture = null;
     }
@@ -123,13 +279,13 @@ export default class Entity {
 
     const extraCssClasses = this.darkMode ? "dark" : "";
 
-    const marker = L.marker(this._latlong(latitude, longitude), {
+    const marker = L.marker(this.latLng, {
       icon: L.divIcon({
         html: `
           <map-card-entity-marker
             entity-id="${this.id}"
-            title="${this._abbr(title ?? "")}"
-            tooltip="${title ?? ""}"
+            title="${this.title}"
+            tooltip="${this.tooltip}"
             icon="${icon ?? ""}"
             picture="${
               picture ?? ""
@@ -149,4 +305,5 @@ export default class Entity {
     });
     return marker;
   }
+
 }
--- src/services/render/EntitiesRenderService.js
+++ src/services/render/EntitiesRenderService.js
@@ -22,8 +22,6 @@ export default class EntitiesRenderService {
   isDarkMode = false;
   /** @type {HaDateRangeService} */
   dateRangeManager;
-  /** @type {object.<string,LayerGroup>} */
-  historyLayerGroups = {};
   /** @type {HaLinkedEntityService} */
   linkedEntityService;
   /** @type {HaHistoryService} */
@@ -43,33 +41,8 @@ export default class EntitiesRenderService {
     this.entities = this.entityConfigs.map((configEntity) => {
       // Attempt to setup entity. Skip on fail, so one bad entity does not affect others.
       try {
-        const stateObj = this.hass.states[configEntity.id];
-        const {
-          //passive,
-          icon: entity_icon,
-          //radius,
-          entity_picture,
-          //gps_accuracy: gpsAccuracy,
-          friendly_name
-        } = stateObj.attributes;
-
-        const state = this.hass.formatEntityState(stateObj);
-
-        // Get lat lng
-        let latLng = this.getEntityLatLng(configEntity.id, this.hass.states);
-        const latitude = latLng[0] ?? null;
-        const longitude = latLng[1] ?? null;
-
-        // If no configured picture, fallback to entity picture
-        let picture = configEntity.picture ?? entity_picture;
-        // Skip if neither found and return null
-        picture = picture ? this.hass.hassUrl(picture) : null;
-
-        // Override icon?
-        let icon = configEntity.icon ?? entity_icon;
-
-        const entity = new Entity(configEntity, latitude, longitude, icon, friendly_name, state, picture, this.isDarkMode);      
-        entity.marker.addTo(this.map);
+        const entity = new Entity(configEntity, this.hass, this.map, this.historyService, this.dateRangeManager, this.linkedEntityService, this.isDarkMode);
+        entity.setup();
         return entity; 
       } catch (e){
         Logger.error("Entity: " + configEntity.id + " skipped due to missing data", e);
@@ -80,134 +53,16 @@ export default class EntitiesRenderService {
     // Remove skipped entities.
     .filter(v => v);
 
-    this.setupHistory()
   }
-
-  async setupHistory() {
-    this.entities.forEach((ent) => {
-      
-      let historyDebug = `History config for: ${ent.id}\n`;
-      
-      if (!ent.hasHistory) {
-        historyDebug += `- Not enabled`;
-        Logger.debug(historyDebug);
-        return;
-      }
-
-      // Setup layer for entities history
-      this.historyLayerGroups[ent.id] = new LayerGroup();
-      this.map.addLayer(this.historyLayerGroups[ent.id]);
-
-      // If entity is using the date range manager.
-      if (ent.config.usingDateRangeManager) {
-        // HaDateRangeService, HaLinkedEntityService and future services should use same structure.
-        this.dateRangeManager.onDateRangeChange((range) => {
-          ent.setHistoryDates(range.start, range.end);
-          this.refreshEntityHistory(ent);
-        });
-
-        historyDebug += `- Using DateRangeManager`;
-        Logger.debug(historyDebug);
-        return;
-      }
-
-      // If have start entity, link it
-      if (ent.config.historyStartEntity) {
-        this.linkedEntityService.onStateChange(
-          ent.config.historyStartEntity,
-          (newState) => {
-            const date = HaMapUtilities.getEntityHistoryDate(newState, ent.config.historyStartEntitySuffix);
-            ent.setHistoryDates(date, ent.currentHistoryEnd);
-            this.refreshEntityHistory(ent);
-          }
-        );
-        historyDebug += `- Start: linked entity "${ent.config.historyStartEntity}"\n`;
-      } else {
-        ent.currentHistoryStart = ent.config.historyStart;
-        historyDebug += `- Start: fixed date ${ent.currentHistoryStart}\n`;
-      }
-
-      // If have end entity, link it.
-      if (ent.config.historyEndEntity) {
-        this.linkedEntityService.onStateChange(
-          ent.config.historyEndEntity,
-          (newState) => {
-            const date = HaMapUtilities.getEntityHistoryDate(newState, ent.config.historyEndEntitySuffix);
-            ent.setHistoryDates(ent.currentHistoryStart, date);
-            this.refreshEntityHistory(ent);
-          }
-        );
-        historyDebug += `- End: linked entity "${ent.config.historyEndEntity}"\n`;
-      } else {
-        ent.currentHistoryEnd = ent.config.historyEnd;
-        historyDebug += `- End: fixed date ${ent.currentHistoryEnd??'now'}\n`;
-      }
-
-      // Provide summary of config for each entities history
-      Logger.debug(historyDebug);
-
-      // Render history now if start is fixed and end isn't dynamic
-      if (ent.config.historyStart && !ent.config.historyEndEntity) {
-        ent.setupHistory(this.historyService, ent.config.historyStart, ent.config.historyEnd);
-      }
-      
-    });
-  }
-
-  refreshEntityHistory(ent) {
-    Logger.debug(`Refreshing history for ${ent.id}: ${ent.currentHistoryStart} -> ${ent.currentHistoryEnd}`);
-    // Remove layer if it already exists.
-    if(this.historyLayerGroups[ent.id]) this.map.removeLayer(this.historyLayerGroups[ent.id]);
-
-    this.historyLayerGroups[ent.id] = new LayerGroup();
-    this.map.addLayer(this.historyLayerGroups[ent.id]);
-
-    // Subscribe new history
-    ent.setupHistory(this.historyService, ent.currentHistoryStart, ent.currentHistoryEnd);
-}
 
   async render() {
     this.entities.forEach((ent) => {
-      const stateObj = this.hass.states[ent.id];
-      // Get location
-      const latLng = this.getEntityLatLng(ent.id, this.hass.states);
-      const latitude = latLng[0] ?? null;
-      const longitude = latLng[1] ?? null;
-
-      ent.update(this.map, latitude, longitude, this.hass.formatEntityState(stateObj));
-
-      ent.renderHistory().forEach((marker) => {
-        marker.addTo(this.historyLayerGroups[ent.id]);
-      });
+      ent.update();
     });
   }
 
-  // Get Lat/Lng of entity. Some entities such as "person" define device_trackers allowing
-  // multiple lat/lng sources to be used. This method will call down through these looking for a
-  // lat/lng value if none is defined on the parent entity.
-  getEntityLatLng(entityId, states) {
-    let entity = states[entityId];
-
-    // Do we have Lng/Lat directly?
-    if (entity.attributes.latitude && entity.attributes.longitude) {
-        return [entity.attributes.latitude, entity.attributes.longitude];
-    }
-
-    // If any, see if we can get a lng/lat from one instead
-    let subTrackerIds = states[entityId]?.attributes?.device_trackers ?? []
-    for(let t=0; t<subTrackerIds.length; t++) {
-      entity = states[subTrackerIds[t]];
-      if (entity.attributes.latitude && entity.attributes.longitude) {
-          return [entity.attributes.latitude, entity.attributes.longitude];
-      }
-    }
-
-    // :(
-    return null;
-  }
-
   setInitialView() {
-    const points = this.entities.filter(e => e.config.focusOnFit).map((e) => e.marker.getLatLng());
+    const points = this.entities.filter(e => e.config.focusOnFit).map((e) => e.latLng);
     // If not, get bounds of all markers rendered
     const bounds = new LatLngBounds(points);
     this.map.fitBounds(bounds.pad(0.1));
