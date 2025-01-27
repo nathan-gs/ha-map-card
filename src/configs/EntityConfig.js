52b3a8fc00746bf1a3ee14b36b40660b38616578
Korn <20152583+k-korn@users.noreply.github.com>
2025-01-27 14:55:03 +0200

    Update _generateRandomColor to produce stable pseudo-random color
    Rewrite _generateRandomColor() to use entity ID as the seed for entity color, thus providing consistent colors for different browser sessions or users.

--- src/configs/EntityConfig.js
+++ src/configs/EntityConfig.js
@@ -164,6 +164,22 @@ export default class EntityConfig {
   }  
 
   _generateRandomColor() {
-    return "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+    // Generate pseudo-random color from provided entity id
+    let str = this.id;
+    // 53-bit hash based on cyrb53 (c) 2018 bryc (github.com/bryc). 
+    // License: Public domain, https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+    let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
+    for(let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    let hash =  4294967296 * (2097151 & h2) + (h1 >>> 0);
+    let color = `hsl(${hash % 360}, 95%, 35%)`;
+    return color;
   }
 }
