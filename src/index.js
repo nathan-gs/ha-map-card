c6074021203d55324e79f3c4a7c51572cc623a20
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-04-09 17:41:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import MapCard from "./components/MapCard.js"
 import MapCardEntityMarker from "./components/MapCardEntityMarker.js"
+import MapCardEntityBadge from "./components/MapCardEntityBadge.js"
 
 if (!customElements.get("map-card")) {
   customElements.define("map-card", MapCard);
   customElements.define("map-card-entity-marker", MapCardEntityMarker);
+  customElements.define("map-card-entity-badge", MapCardEntityBadge);
   console.info(
     `%cnathan-gs/ha-map-card: HA_MAP_CARD_VERSION`,
     'color: orange; font-weight: bold; background: black'
