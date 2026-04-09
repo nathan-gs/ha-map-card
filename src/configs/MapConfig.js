2069f067ce6b0ae95666b94131ab119ea6295308
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2026-04-09 17:39:19 +0300

    Update MapConfig.js

--- src/configs/MapConfig.js
+++ src/configs/MapConfig.js
@@ -64,7 +64,7 @@ export default class MapConfig {
     this.themeMode = ['dark', 'light', 'auto'].includes(inputConfig.theme_mode) ? inputConfig.theme_mode : 'auto';
 
     // Enable marker clustering (default: true)
-    this.clusterMarkers = this._setConfigWithDefault(inputConfig.cluster_markers, true);
+    this.clusterMarkers = this._setConfigWithDefault(inputConfig.cluster_markers, false);
 
     // Enable debug messaging. 
     // Card is quite chatty with this enabled.
@@ -134,7 +134,7 @@ export default class MapConfig {
   }
 
   _setConfigWithDefault(input, d = null) {
-    if (!input) {
+    if (input === undefined || input === null) {
       if (d == null) {
         throw new Error("Missing key ");
       }
